cb342775bdcc3887a261cab30f5fb1f470488f38
Luciana Nunes <lcnunes09@gmail.com>
2020-09-02 22:31:59 -0300

    Fix routes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -25,12 +25,12 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <a href="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
                     </a>
 
-                    <a href="" className="give-classes">
+                    <a href="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas"/>
                         Dar aulas
                     </a>
