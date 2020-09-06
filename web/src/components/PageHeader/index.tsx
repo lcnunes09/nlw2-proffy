862f156f8d95b0061a4ba8eb416472de8883cca8
Luciana Nunes <lcnunes09@gmail.com>
2020-09-06 16:00:49 -0300

    Criar página Teacher Form

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,20 +8,23 @@ import './styles.css';
 
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
-const PageHeader:React.FC<PageHeaderProps> = (props) => {
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className="page-header">
             <div className="top-bar-container">
                 <Link to="/">
-                    <img src={backIcon} alt="Voltar"/>
+                    <img src={backIcon} alt="Voltar" />
                 </Link>
-                <img src={logoImg} alt="Proffy"/>
+                <img src={logoImg} alt="Proffy" />
             </div>
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+
+                {props.description && <p>{props.description}</p>}
 
                 {props.children}
             </div>
