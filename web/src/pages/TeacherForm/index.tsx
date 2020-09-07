566864134691c3081b70e6444f65faa49ca12508
Luciana Nunes <lcnunes09@gmail.com>
2020-09-07 01:43:57 -0300

    Redirect after submit signup form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,16 +1,19 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Textarea from '../../components/Textarea';
 import Select from '../../components/Select';
+import api from '../../services/api';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
-import api from '../../services/api';
 
 function TeacherForm() {
+    const history = useHistory();
+
     const [name, setName] = useState('');
     const [avatar, setAvatar] = useState('');
     const [whatsapp, setWhatsapp] = useState('');
@@ -59,6 +62,7 @@ function TeacherForm() {
             schedule: scheduleItems
         }).then(() => {
             alert('Cadastro realizado com sucesso!');
+            history.push('/');
         }).catch(() => {
             alert('Erro no cadastro!');
         })
