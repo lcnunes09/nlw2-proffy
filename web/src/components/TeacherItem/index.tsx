86228a5931539a2115aaf46256d93672d6bbe5a0
Luciana Nunes <lcnunes09@gmail.com>
2020-09-14 22:48:52 -0300

    Add teacher list and interact search bar

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,35 +4,45 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/14111673?s=460&u=e522a6fce2e6b5a403fc647f23437ec6052df1f5&v=4" alt="Luciana Nunes"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Luciana Nunes</strong>
-                    <span>Matéria</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                <br /> <br />
-                Deleniti incidunt perferendis temporibus doloremque natus dignissimos magnam nesciunt consequuntur enim praesentium quasi, dolore aspernatur quas nemo eveniet blanditiis minus ipsam mollitia.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 50,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type="button">
-                    <img src={whatsappIcon} alt="WhatsApp Icon"/>
+                <a href={`https://wa.me/${teacher.whatsapp}`}>
+                    <img src={whatsappIcon} alt="WhatsApp Icon" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
-        </article>
+        </article >
     )
 }
 
